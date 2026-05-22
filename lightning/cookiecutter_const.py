7abefe2fa07e82de3fc3c9e022092a9bb3ef382b
Your Name <you@example.com>
2026-05-22 11:47:05 +0300

    01

---
+++ lightning/cookiecutter_const.py
@@ -0,0 +1,8 @@
+# PREP {
+STUDENT_FULL_NAME_RUS = "Граблевский Михаил Владимирович"
+PROJECT_NAME_RUS = "Молния"
+PROJECT_DESCRIPTION =  "Информационная система таксопарка"
+MAIN_BACKGROUND_COLOR = "#FFFFFF"
+AUX_BACKGROUND_COLOR = "#C0C0C0"
+ATTENTION_COLOR = "#FF00FF"
+# } PREP
---
+++ lightning/secret.py
@@ -0,0 +1,3 @@
+DB_NAME = "grablevskiy_mv_lightning"
+DB_USER = "User"
+DB_PASS = "User"
