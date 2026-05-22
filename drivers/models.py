7cc54835d0f028b9cbb83f23fcbfe0c252749b2d
Your Name <you@example.com>
2026-05-22 13:43:49 +0300

    04

--- drivers/models.py
+++ drivers/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Driver(models.Model):
+    first_name = models.CharField(max_length=100, verbose_name="Имя")
+    patronymic_name = models.CharField(max_length=100, verbose_name="Отчество")
+    last_name = models.CharField(max_length=100, verbose_name="Фамилия")
+    phone = models.PositiveBigIntegerField(verbose_name="Телефон")
+    birthday = models.DateField(verbose_name="Дата рождения")
