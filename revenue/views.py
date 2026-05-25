205f68d18d0d690b75814465fba88332327694f3
Your Name <you@example.com>
2026-05-25 15:09:56 +0300

    31

--- revenue/views.py
+++ revenue/views.py
@@ -5,3 +5,7 @@ from revenue.models import Revenue
 
 class RevenueListView(ListView):
     model = Revenue
+
+    def get_queryset(self):
+        queryset = Revenue.objects.filter(driver__pk=self.kwargs.get("pk"))
+        return queryset
