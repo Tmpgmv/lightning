f80b8606598fd4088414accd9039fc4b56c18784
Your Name <you@example.com>
2026-05-25 14:19:15 +0300

    20

--- drivers/views.py
+++ drivers/views.py
@@ -11,4 +11,4 @@ class DriverCreateView(SuccessMessageMixin,
     model = Driver
     fields = "__all__"
     success_url = reverse_lazy("home")
-    success_message = "Информация о водителе добавлена"
+    success_message = "Информация о водителе сохранена"
