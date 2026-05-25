ce5b27e6a5bf11b531eabf494b885df986aa793b
Your Name <you@example.com>
2026-05-25 16:01:29 +0300

    36

--- revenue/models.py
+++ revenue/models.py
@@ -5,3 +5,6 @@ class Revenue(models.Model):
     datetime = models.DateTimeField(verbose_name="Дата и время")
     amount = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0)])
     driver = models.ForeignKey("drivers.Driver", on_delete=models.CASCADE, verbose_name="Водитель")
+
+    class Meta:
+        ordering = ["-datetime"]
