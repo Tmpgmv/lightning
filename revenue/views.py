566d1fb7d4bd99b4e678f956bbeccea328cf8ff0
Your Name <you@example.com>
2026-05-25 14:51:53 +0300

    29

--- revenue/views.py
+++ revenue/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from revenue.models import Revenue
+
+
+class RevenueListView(ListView):
+    model = Revenue
