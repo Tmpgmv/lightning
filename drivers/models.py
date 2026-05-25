db9719a2f9868f0ea0da2f1984146b519d5e856a
Your Name <you@example.com>
2026-05-25 10:30:56 +0300

    15

--- drivers/models.py
+++ drivers/models.py
@@ -11,5 +11,12 @@ class Driver(models.Model):
     def __str__(self):
         return f"{self.last_name} {self.first_name} {self.patronymic_name}"
 
+    def get_phone(self):
+        return "{}-{}-{}-{}-{}".format(str(self.phone)[0],
+                                       str(self.phone)[1:4],
+                                       str(self.phone)[4:7],
+                                       str(self.phone)[7:9],
+                                       str(self.phone)[9:11], )
+
     class Meta:
         ordering = ["last_name", "first_name", ]
