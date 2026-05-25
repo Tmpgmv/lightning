ef5af865f9b77e7e0a58cefa720465c8dfa579b2
Your Name <you@example.com>
2026-05-25 13:40:44 +0300

    19

--- drivers/views.py
+++ drivers/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-# Create your views here.
+from drivers.models import Driver
+
+
+class DriverCreateView(SuccessMessageMixin,
+                       CreateView):
+    model = Driver
+    fields = "__all__"
+    success_url = reverse_lazy("home")
+    success_message = "Информация о водителе добавлена"
